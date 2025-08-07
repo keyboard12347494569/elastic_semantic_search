6868d15d7ac3f430707a8bc97390b1ae89ed03ef
ajeet <ajeet214.90@gmail.com>
2025-08-07 20:43:24 +0700

    fetch index from .env

--- app/schema.py
+++ app/schema.py
@@ -1,11 +1,19 @@
 # app/schema.py
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+ES_INDEX = os.getenv("ES_INDEX", "people-index")
+
 """
 It defines a JSON schema for validating the DSL query structure — ensuring index, query, and optional fields like size, aggs, etc., are well-formed.
 """
 DSL_SCHEMA = {
   "type": "object",
   "properties": {
-    "index": {"type": "string", "enum": ["people-index"]},
+    "index": {"type": "string", "enum": [ES_INDEX]},
     "query": {"type": "object"},
     "aggs":  {"type": "object"},
     "size":  {"type": "integer", "minimum": 0, "maximum": 200},
